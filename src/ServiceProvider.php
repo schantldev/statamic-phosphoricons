6cf831311d7c273e35af58383ed3e0f09d184b1a
Andreas Schantl <andreas@schantl.io>
2025-05-01 15:03:40 +0200

    Fix for #1

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace SchantlDev\StatamicPhosphoricons;
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Statamic\Fieldtypes\Sets;
 use Statamic\Providers\AddonServiceProvider;
 
@@ -21,8 +22,17 @@ class ServiceProvider extends AddonServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/phosphoricons.php', 'phosphoricons');
 
         if (!$this->app->runningInConsole()) {
+            File::ensureDirectoryExists(resource_path('svg'));
+
             if (! File::exists(resource_path('svg/phosphoricons'))) {
                 File::link(__DIR__.'/../assets', resource_path('svg/phosphoricons'));
+
+                // add directory link to .gitignore to prevent problems with deployments
+                if (! File::exists(resource_path('svg/.gitignore'))) {
+                    File::put(resource_path('svg/.gitignore'), '!.gitignore'.PHP_EOL.'phosphoricons');
+                } else if (! Str::of(File::get(resource_path('svg/.gitignore')))->contains('phosphoricons')) {
+                    File::append(resource_path('svg/.gitignore'), PHP_EOL.'phosphoricons');
+                }
             }
 
             Sets::setIconsDirectory(resource_path('svg/phosphoricons'), config('phosphoricons.variant', 'regular'));
